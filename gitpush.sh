edc9137e9e5fcffe278db64e503e5846e94996c9
islamrahat <islamrahat701@gmail.com>
2026-04-18 13:25:16 +0600

    2026-04-18 13:25:16

---
+++ gitpush.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+cp -r ~/.config/waybar/. waybar/.
+cp -r ~/.config/hypr/. hypr/.
+cp -r ~/.config/alacritty/. alacritty/.
+
+git init
+git add .
+git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
+git push -u origin main
